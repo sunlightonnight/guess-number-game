6eb20cb84bf6ded0ccb332f5e7cf35f8526031f3
sunlightonnight <sunlightonnight@gmail.com>
2026-06-13 21:22:54 +0300

    Update guess_number.py
    Add random number generation

--- guess_number.py
+++ guess_number.py
@@ -1,16 +1,24 @@
-number = 5
+import random
+
+number = random.randint(1, 10)
 attempts = 0
 max_attempts = 3
+
+print("Угадай число от 1 до 10. У тебя 3 попытки.")
+
 while attempts < max_attempts:
-    guess = int(input("Введите число от 1 до 10:"))
+    guess = int(input("Введите число: "))
     attempts += 1
+    
     if guess == number:
-        print("Вы угадали число!")
+        print(f"Поздравляю! Ты угадал за {attempts} попытки(у)!")
         break
     elif guess < number:
-        print("Загаданное число больше.")
-    elif guess > number:
-        print("Загаданное число меньше.")
-    print(f"Осталось {max_attempts - attempts} попыток.")
+        print("Загаданное число БОЛЬШЕ")
+    else:
+        print("Загаданное число МЕНЬШЕ")
+    
+    print(f"Осталось попыток: {max_attempts - attempts}")
+
 if attempts == max_attempts and guess != number:
-    print(f"У вас закончились попытки, было загадано число {number}")
+    print(f"Ты проиграл! Было загадано число {number}")
